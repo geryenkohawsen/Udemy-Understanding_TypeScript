398576da37b0f469b6d32a116fa653ad45ac405c
Geryenko Hawsen <gery.zarons@gmail.com>
2023-06-12 23:29:04 +0900

    20. Working with Enums

--- app.ts
+++ app.ts
@@ -13,16 +13,27 @@
 //   age: 20,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'Gery',
+//   age: 20,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+enum Role {ADMIN, READ_ONLY, AUTHOR} 
+enum Role2 {ADMIN = 5, READ_ONLY, AUTHOR} // can choose your own starting number
+enum Role3 {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'xyz'} // can use string also
+
+const person = {
   name: 'Gery',
   age: 20,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 // person.role.push('admin')
@@ -37,3 +48,5 @@ for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); // Error because TypeScript inferences knows that the map() method are not callable from a string
 }
+
+if (person.role === Role.ADMIN) console.log('Role is ADMIN!');
