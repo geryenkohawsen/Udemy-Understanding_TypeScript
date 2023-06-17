c950ae21dd7a8f46ea9a1e0ca5eecc2b549a0d21
Geryenko Hawsen <gery.zarons@gmail.com>
2023-06-18 00:30:52 +0900

    28. Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -2,13 +2,18 @@ function add(n1: number, n2: number): number {
   return n1 + n2;
 }
 
-function printResult(num: number) {
+function printResult(num: number): void {
   console.log('Result: ' + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+  cb(result)
 }
 
 printResult(add(5, 12));
 
-let combineValues: (a: number, b: number) => number);
+let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 // combineValues = printResult;
@@ -16,3 +21,7 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 // let test: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
