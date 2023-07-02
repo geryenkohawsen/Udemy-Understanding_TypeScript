79f7157ba862f079cb43422f546b1f914a7e9106
Geryenko Hawsen <gery.zarons@gmail.com>
2023-07-02 19:31:56 +0900

    34. Using "Watch Mode"

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-var userInput; // any is the most flexible type, it disable all type checking
-var userName;
-userInput = 5;
-userInput = 'Max';
-// unknown is better than any because it still enable type checking
-// if you know what to do with a data eventually without knowing the actually type of the data, unknown is better
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('Error', 500);
-console.log(result);
+var userName = 'Gery';
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,18 +1,3 @@
-let userInput: unknown; // any is the most flexible type, it disable all type checking
-let userName: string;
+const userName = 'Gery';
 
-userInput = 5;
-userInput = 'Max';
-
-// unknown is better than any because it still enable type checking
-// if you know what to do with a data eventually without knowing the actually type of the data, unknown is better
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never { // this will NEVER return anything because it will break/crash our script
-  throw { message: message, errorCode: code };
-}
-
-const result = generateError('Error', 500);
-console.log(result);
+console.log(userName);
