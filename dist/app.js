3544986fb46e8ae853b5723180dd9fe0f724b482
Geryenko Hawsen <gery.zarons@gmail.com>
2023-07-12 00:11:57 +0900

    44. Code Quality Options

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,10 @@
 "use strict";
+let appId = 'abc';
 const button = document.querySelector('button');
+function clickHandler(message) {
+    let username = "unused";
+    console.log('Clicked !' + message);
+}
 if (button) {
-    button.addEventListener('click', () => {
-        console.log('Clicked!');
-    });
+    button.addEventListener('click', clickHandler.bind(null, 'You are welcome!'));
 }
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+let appId = 'abc';
 const button = document.querySelector('button');
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 >= 0) {
+    return n1 + n2;
+  }
+  // return;
+}
+
+function clickHandler(message: string) {
+  let username = 'unused';
+  console.log('Clicked !' + message);
+}
 
 // this is a button
 if (button) {
-  button.addEventListener('click', () => {
-    console.log('Clicked!');
-  });
+  button.addEventListener('click', clickHandler.bind(null, 'You are welcome!'));
 }
