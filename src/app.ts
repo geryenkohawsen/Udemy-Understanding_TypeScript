bdacecaec581c818b770ca8ef0d2fe12a214652c
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-01 23:28:58 +0900

    53. Rest Parameters

--- src/app.ts
+++ src/app.ts
@@ -18,18 +18,18 @@ import { Agent } from 'http';
 // }
 // console.log(isOld); // Error in TS but not in JS
 
-const add = (a: number, b: number = 1) => a + b; // always set default argument on the right because JS will not skip the first default argument
+// const add = (a: number, b: number = 1) => a + b; // always set default argument on the right because JS will not skip the first default argument
 
-const printOutput = (output: string | number) => console.log(output);
+// const printOutput = (output: string | number) => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-  button.addEventListener('click', (event) => console.log(event));
-}
+// if (button) {
+//   button.addEventListener('click', (event) => console.log(event));
+// }
 
-printOutput(add(5, 2));
-printOutput(add(1));
+// printOutput(add(5, 2));
+// printOutput(add(1));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
@@ -43,3 +43,12 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 101, 2, 3, 3.7);
+console.log(addedNumbers);
