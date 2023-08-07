ee6ef9938687e2987e9028fa81526511f6105f69
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-07 23:56:52 +0900

    62. "private" and "public" Access Modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 class Department {
   // name: string = 'DEFAULT'; // an initializer value is not mandatory
-  name: string;
+  public name: string; // public keyword is the DEFAULT setting
+  private employees: string[] = [];
 
   // a method that is executed when the object is initiated
   constructor(n: string) {
@@ -11,13 +12,28 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Tom');
+
+accounting.employees[2] = 'Anna';
+
 console.log(accounting);
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'Dummy', describe: accounting.describe };
+// const accountingCopy = { name: 'Dummy', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
