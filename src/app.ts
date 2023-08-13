e24ee8423bd897418ef77e5bf4adf284e1294e6d
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-13 15:15:43 +0900

    63. Shorthand Initialization

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 class Department {
   // name: string = 'DEFAULT'; // an initializer value is not mandatory
-  public name: string; // public keyword is the DEFAULT setting
+  // private id: string;
+  // private name: string; // public keyword is the DEFAULT setting
   private employees: string[] = [];
 
   // a method that is executed when the object is initiated
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   // Make sure to catch unwanted behavior in TS
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log('Department: ' + this.id + this.name);
   }
 
   addEmployee(employee: string) {
@@ -23,12 +25,12 @@ class Department {
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Tom');
 
-accounting.employees[2] = 'Anna';
+// accounting.employees[2] = 'Anna';
 
 console.log(accounting);
 accounting.describe();
