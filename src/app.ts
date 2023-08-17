4006e93732847b50574e92a5c008a0155f6b4b27
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-17 23:59:30 +0900

    73. Using Interfaces with Classes

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,37 @@
 /**
- * interface describe the structure of an object
+ * 1. interface describe the structure of an object
+ * 2. interface and custom type are not the same. Type can store union types but interface can be implemented by a class
  */
 
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
+  // age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: 'Max',
-  age: 30,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person('Max');
+
+// user1 = {
+//   name: 'Max',
+//   age: 30,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   },
+// };
 
 user1.greet('HI! I am');
