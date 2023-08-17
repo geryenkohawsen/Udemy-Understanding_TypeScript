0f8b35a291ff2bfee1d1192e28d106e9621198ef
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-18 01:04:24 +0900

    78. Optional Parameters & Properties

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,8 @@ add = (n1: number, n2: number) => {
  */
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Aged {
@@ -34,21 +35,28 @@ interface Greetable extends Named, Aged {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
 
 user1 = new Person('Max');
+const user2 = new Person();
 // user1.name = 'Manu';
 
 // user1 = {
@@ -60,3 +68,4 @@ user1 = new Person('Max');
 // };
 
 user1.greet('HI! I am');
+user2.greet();
