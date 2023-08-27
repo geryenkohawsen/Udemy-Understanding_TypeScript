fd44a381bab7e7e5eb717d7fb59c584917867e2d
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-28 00:16:53 +0900

    85. Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -72,3 +72,32 @@ function useVehicle(vehicle: Vehicle) {
 		vehicle.loadCargo(1000);
 	}
 }
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+	type: 'bird';
+	flyingSpeed: number;
+}
+
+interface Horse {
+	type: 'horse';
+	runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+	let speed;
+	switch (animal.type) {
+		case 'bird':
+			speed = animal.flyingSpeed;
+			break;
+		case 'horse':
+			speed = animal.runningSpeed;
+	}
+	console.log('Moving speed --> ', speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
