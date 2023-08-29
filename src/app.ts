b2774c485b0052551f7e30882ed1d4cad65d16a2
Geryenko Hawsen <gery.zarons@gmail.com>
2023-08-29 23:18:22 +0900

    88. Function Overloads

--- src/app.ts
+++ src/app.ts
@@ -22,12 +22,18 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
 	if (typeof a === 'string' || typeof b === 'string') {
 		return a.toString() + b.toString();
 	}
 	return a + b;
 }
+
+const result = add('Geryenko', 'Hawsen');
 
 type UnknownEmployee = Employee | Admin;
 
