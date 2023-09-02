ef98c1c1f8b308dae72f5fad87ccfa0bd8ac1f9f
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-02 20:20:06 +0900

    95. Creating a Generic Function

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
-// the Array is the first built-in generic type of TS
+// // the Array is the first built-in generic type of TS
 
-const names: Array<string> = [];
-// names[0].split(' ');
+// const names: Array<string> = [];
+// // names[0].split(' ');
 
-//  second is the Promise
-const promise: Promise<number> = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve(0);
-	}, 2000);
-});
+// //  second is the Promise
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve(0);
+// 	}, 2000);
+// });
 
-promise.then((data) => {
-	// data.split(' ');
-});
+// promise.then((data) => {
+// 	// data.split(' ');
+// });
+
+function merge<T extends object, U>(objA: T, objB: U) {
+	return Object.assign(objA, objB);
+}
+
+const mergedObj1 = merge({ name: 'Gery', hobbies: ['Sports'] }, { age: 24 });
+const mergedObj2 = merge({ name: 'Gery' }, { age: 24 });
+
+console.log('mer1 --> ', mergedObj1.hobbies);
+// console.log('mer2 --> ', mergedObj2.hobbies); // no hobbies property
