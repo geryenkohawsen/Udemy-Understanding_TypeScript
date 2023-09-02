a2ce912c14bb6313dee208cc5c70cdd66d56f142
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-02 20:45:06 +0900

    97. Another Generic Function

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,26 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 	return Object.assign(objA, objB);
 }
 
-const mergedObj1 = merge({ name: 'Gery', hobbies: ['Sports'] }, 24);
+const mergedObj1 = merge({ name: 'Gery', hobbies: ['Sports'] }, { age: 24 });
 const mergedObj2 = merge({ name: 'Gery' }, { age: 24 });
 
 console.log('mer1 --> ', mergedObj1);
 // console.log('mer2 --> ', mergedObj2.hobbies); // no hobbies property
+
+interface Lengthy {
+	length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+	let descriptionText = 'Got no value.';
+	if (element.length > 0) {
+		descriptionText = `Got ${element.length} element${
+			element.length > 1 ? 's' : ' '
+		}.`;
+	}
+	return [element, descriptionText];
+}
+
+console.log('countAndDescribe --> ', countAndDescribe('Hi there!'));
+console.log('countAndDescribe --> ', countAndDescribe(['Sports', 'Cooking']));
+console.log('countAndDescribe --> ', countAndDescribe([]));
