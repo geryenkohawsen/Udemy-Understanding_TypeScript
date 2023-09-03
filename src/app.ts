ef999195f365985261dcdf168ae37ec20e3c3181
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-03 23:40:41 +0900

    107. Building More Useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,20 @@ function Logger(logString: string) {
 	};
 }
 
+function WithTemplate(template: string, hookId: string) {
+	return function (constructor: any) {
+		const hookEl = document.getElementById(hookId);
+		const p = new constructor();
+		if (hookEl) {
+			hookEl.innerHTML = template;
+			hookEl.querySelector('h1')!.textContent = p.name;
+		}
+	};
+}
+
 // decorator are launch when your class is DEFINED not when it is INSTANTIATED
-@Logger('LOGING - PERSON')
+// @Logger('LOGING - PERSON')
+@WithTemplate('<h1>###WithTemplate###</h1>', 'app')
 class Person {
 	name = 'Gery';
 
