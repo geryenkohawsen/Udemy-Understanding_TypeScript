789a40436dbbbfd5663c7b6bd69c8ac9def5dda7
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-05 23:56:48 +0900

    112. Returning (and changing) a Class in a Class Decorator

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,22 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log('TEMPLATE FACTORY');
-  return function (constructor: any) {
-    console.log('Rendering template');
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    // What we can from a decorator function depends on the type of decorator
+    return class extends originalConstructor {
+      // extend to keep the original properties
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -33,9 +41,9 @@ class Person {
   }
 }
 
-const pers = new Person();
+// const pers = new Person();
 
-console.log('pers --> ', pers);
+// console.log('pers --> ', pers);
 
 //  --
 
