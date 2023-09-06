e05f7a9fc559e4954882a417499489d2698af424
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-07 00:41:08 +0900

    114. Example: Creating an "Autobind" Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,31 @@
 // Most decorator use uppercase for the starting letter
 function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
-  return function (constructor: Function) {
-    console.log('logString', logString);
-    console.log('constructor --> ', constructor);
-  };
+	console.log('LOGGER FACTORY');
+	return function (constructor: Function) {
+		console.log('logString', logString);
+		console.log('constructor --> ', constructor);
+	};
 }
 
 function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY');
-  return function <T extends { new (...args: any[]): { name: string } }>(
-    originalConstructor: T
-  ) {
-    // What we can from a decorator function depends on the type of decorator
-    return class extends originalConstructor {
-      // extend to keep the original properties
-      constructor(..._: any[]) {
-        super();
-        console.log('Rendering template');
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector('h1')!.textContent = this.name;
-        }
-      }
-    };
-  };
+	console.log('TEMPLATE FACTORY');
+	return function <T extends { new (...args: any[]): { name: string } }>(
+		originalConstructor: T
+	) {
+		// What we can from a decorator function depends on the type of decorator
+		return class extends originalConstructor {
+			// extend to keep the original properties
+			constructor(..._: any[]) {
+				super();
+				console.log('Rendering template');
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector('h1')!.textContent = this.name;
+				}
+			}
+		};
+	};
 }
 
 // decorator are launch when your class is DEFINED not when it is INSTANTIATED
@@ -34,11 +34,11 @@ function WithTemplate(template: string, hookId: string) {
 @Logger('LOGING - PERSON')
 @WithTemplate('<h1>###WithTemplate###</h1>', 'app')
 class Person {
-  name = 'Gery';
+	name = 'Gery';
 
-  constructor() {
-    console.log('Creating person object...');
-  }
+	constructor() {
+		console.log('Creating person object...');
+	}
 }
 
 // const pers = new Person();
@@ -50,71 +50,98 @@ class Person {
 //  when adding a decorator to a property, it needs 2 arguments
 //  Again, the constructor will execute when the property is DEFINED
 function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!!');
-  console.log(target);
-  console.log(propertyName);
+	console.log('Property decorator!!');
+	console.log(target);
+	console.log(propertyName);
 }
 
 // getter setter decorator (receive 3 arguments), also can return a new PropertyDescriptor.
 function Log2(
-  target: any,
-  name: string,
-  descriptor: PropertyDescriptor
+	target: any,
+	name: string,
+	descriptor: PropertyDescriptor
 ): PropertyDescriptor {
-  console.log('Accessor decorator');
-  console.log('target → ', target);
-  console.log('name → ', name);
-  console.log('descriptor → ', descriptor);
-  return {
-    enumerable: true,
-  };
+	console.log('Accessor decorator');
+	console.log('target → ', target);
+	console.log('name → ', name);
+	console.log('descriptor → ', descriptor);
+	return {
+		enumerable: true,
+	};
 }
 
 // method decorator (receive 3 arguments), also can return a new PropertyDescriptor.
 function Log3(
-  target: any,
-  name: string | Symbol,
-  descriptor: TypedPropertyDescriptor<(tax: number) => number>
+	target: any,
+	name: string | Symbol,
+	descriptor: TypedPropertyDescriptor<(tax: number) => number>
 ) {
-  console.log('Method decorator');
-  console.log('target → ', target);
-  console.log('name → ', name);
-  console.log('descriptor → ', descriptor);
+	console.log('Method decorator');
+	console.log('target → ', target);
+	console.log('name → ', name);
+	console.log('descriptor → ', descriptor);
 }
 
 // parameter decorator (receive 3 arguments)
 function Log4(target: any, name: string | Symbol, position: number) {
-  console.log('Parameter decorator');
-  console.log('target → ', target);
-  console.log('name → ', name);
-  console.log('descriptor → ', position);
+	console.log('Parameter decorator');
+	console.log('target → ', target);
+	console.log('name → ', name);
+	console.log('descriptor → ', position);
 }
 
 class Product {
-  @Log
-  title: string;
-  private _price: number;
+	@Log
+	title: string;
+	private _price: number;
 
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error('Invalid price - should be positive');
-    }
-  }
+	@Log2
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val;
+		} else {
+			throw new Error('Invalid price - should be positive');
+		}
+	}
 
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
 
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 + tax);
-  }
+	@Log3
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
+	}
 }
 
 // Decorator are executed when a class is defined, NOT runtime
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book2', 39);
+
+function AutoBind(_: any, _2: string | Symbol, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	console.log('originalMethod --> ', originalMethod, _);
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+class Printer {
+	message = 'This works';
+
+	@AutoBind
+	showMessage() {
+		console.log(this.message);
+	}
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button');
+button?.addEventListener('click', p.showMessage);
