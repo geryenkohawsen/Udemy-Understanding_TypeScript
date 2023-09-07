21352164c3b5d031ca0157987c6bfaf7fbeb47e1
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-07 23:52:37 +0900

    116. Validation with Decorators - Finished

--- src/app.ts
+++ src/app.ts
@@ -146,16 +146,55 @@ const p = new Printer();
 const button = document.querySelector('button');
 button?.addEventListener('click', p.showMessage);
 
-function Required() {}
+// --
 
-function PositiveNumber() {}
+interface ValidatorConfig {
+	[property: string]: {
+		[validatableProp: string]: string[]; // ['required', 'positive']
+	};
+}
 
-function validate(obj: object) {}
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+	registeredValidators[target.constructor.name] = {
+		...registeredValidators[target.constructor.name],
+		[propName]: ['required'],
+	};
+}
+
+function PositiveNumber(target: any, propName: string) {
+	registeredValidators[target.constructor.name] = {
+		...registeredValidators[target.constructor.name],
+		[propName]: ['positive'],
+	};
+}
+
+function validate(obj: any) {
+	const objValidatorConfig = registeredValidators[obj.constructor.name];
+	if (!objValidatorConfig) {
+		return true;
+	}
+	let isValid = true;
+	for (const prop in objValidatorConfig) {
+		for (const validator of objValidatorConfig[prop]) {
+			switch (validator) {
+				case 'required':
+					isValid = isValid && !!obj[prop];
+					break;
+				case 'positive':
+					isValid = isValid && obj[prop] > 0;
+					break;
+			}
+		}
+	}
+	return isValid;
+}
 
 class Course {
 	@Required
 	title: string;
-	@Required
+	@PositiveNumber
 	price: number;
 
 	constructor(t: string, p: number) {
