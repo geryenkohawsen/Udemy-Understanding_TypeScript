cf2ecf46b17beac4f0d271d45860e809e65805d5
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-17 11:22:36 +0900

    125. Fetching User Input

--- src/app.ts
+++ src/app.ts
@@ -50,10 +50,38 @@ class ProjectInput {
 		this.attach();
 	}
 
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInputElement.value;
+		const enteredDescription = this.descriptionInputElement.value;
+		const enteredPeople = this.peopleInputElement.value;
+
+		if (
+			enteredTitle.trim().length === 0 ||
+			enteredDescription.trim().length === 0 ||
+			enteredPeople.trim().length === 0
+		) {
+			alert('Invalid input, please try again!');
+			return;
+		} else {
+			return [enteredTitle, enteredDescription, +enteredPeople];
+		}
+	}
+
+	private clearInput() {
+		this.titleInputElement.value = '';
+		this.descriptionInputElement.value = '';
+		this.peopleInputElement.value = '';
+	}
+
 	@AutoBind
 	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleInputElement.value);
+		const userInput = this.gatherUserInput();
+		if (Array.isArray(userInput)) {
+			const [title, description, people] = userInput;
+			console.log('userInput --> ', title, description, people);
+			this.clearInput();
+		}
 	}
 
 	private configure() {
