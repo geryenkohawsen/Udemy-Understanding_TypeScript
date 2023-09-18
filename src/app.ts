c0fb47cf23de9b41c76e55597b205cc4e6c56b0c
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-18 12:42:01 +0900

    133. Using a Getter

--- src/app.ts
+++ src/app.ts
@@ -166,6 +166,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    if (this.project.numOfPeople === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.numOfPeople} persons`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -178,8 +186,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent(): void {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent =
-      this.project.numOfPeople.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
