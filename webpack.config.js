4aed2b1f8bad6bd98128e92e5242b864d7ff9c2f
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-21 19:31:17 +0900

    156. Adjust Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
   },
+
   devtool: 'inline-source-map',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+
   module: {
     rules: [
       {
