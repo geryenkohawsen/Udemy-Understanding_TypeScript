484a261061e63fe02f290866705399caef59b0b5
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-23 15:23:56 +0900

    165. TypeScript-embracing: class-validator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 import { Product } from './product.model';
 
 const products = [
@@ -12,6 +13,16 @@ const products = [
     price: 25.0,
   },
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERROR');
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product('Book', 19.99);
 
