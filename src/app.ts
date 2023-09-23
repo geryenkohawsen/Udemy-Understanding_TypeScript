b30d9946c1337198e2dd3dafd6f6004090ad04e9
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-23 17:08:18 +0900

    174. Working with Maps without a Credit Card

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,21 @@
-import axios from 'axios';
-
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('a')! as HTMLInputElement;
-
-const GOOGLE_API_KEY = '<KEY>'; // fake key
+declare var ol: any;
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
 
-  // send this to Google's API!
-  type GoogleGeocodingResponse = {
-    results: {
-      geometry: {
-        location: {
-          lat: number;
-          lng: number;
-        };
-      };
-    }[];
-    status: 'OK' | 'ZERO_RESULTS';
-  };
-  axios
-    .get<GoogleGeocodingResponse>(
-      `http://maps.google.com/maps/api/js?address=${encodeURI(
-        enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
-    )
-    .then((response) => {
-      if (response.data.status !== 'OK') {
-        throw new Error('Could not fetch location!');
-      }
-      const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(
-        document.getElementById('map') as HTMLElement,
-        {
-          center: coordinates,
-          zoom: 16,
-        }
-      );
+  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
 
-      new google.maps.Marker({ position: coordinates, map: map });
-    })
-    .catch((error) => {
-      alert(error.message);
-      console.log(error);
-    });
+  document.getElementById('map')!.innerHTML = ''; // clear <p> from <div id="map">
+  new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16,
+    }),
+  });
 }
-
-form.addEventListener('submit', searchAddressHandler);
