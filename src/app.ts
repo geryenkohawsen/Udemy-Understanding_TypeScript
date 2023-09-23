c0678db5bd31c6ca0b563973eeb0db8322aca2d6
Geryenko Hawsen <gery.zarons@gmail.com>
2023-09-23 14:53:45 +0900

    162. Using JavaScript (!) Libraries with TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import _ from 'lodash';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+console.log(_.shuffle([1, 2, 3]));
